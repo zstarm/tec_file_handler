9d20a4d20ac87c0162f5983b57a38689a55dd52b
Zach Starman <starman.zach@gmail.com>
2024-12-19 13:27:20 -0600

    updated tec::variable class to construct new instances with an existing std::vector of data or add an existing std::vector of data to an existing variable given a zone index

--- include/tec_variable.h
+++ include/tec_variable.h
@@ -15,18 +15,25 @@ namespace tec {
 
 		friend class asciiReader;
 		//friend class asciiWriter;
-
-		//friend class szlReader;
+		
+		friend class szlReader;
 		//friend class szlWriter;
 
-
 		std::string name;
-		
 		std::vector<zoneData> subzoneData;
-
+		
+		void add_emptyInstances(int numInstances);
+		void resize_zone(int zone, int _size, dataTypeFlag T = dataTypeFlag::singlePrecision);
+		
 		public:
 			variable();
 			variable(std::string vname);
+			variable(std::string vname, std::vector<float> &&fdata, int zidx = 0);
+			variable(std::string vname, std::vector<double> &&ddata, int zidx = 0);
+			variable(std::string vname, std::vector<int32_t> &&ldata, int zidx = 0);
+			variable(std::string vname, std::vector<int16_t> &&sdata, int zidx = 0);
+			variable(std::string vname, std::vector<uint8_t> &&bdata, int zidx = 0);
+			
 			variable(const variable &obj) = default;
 			variable(variable &&obj) = default;
 			~variable();
@@ -35,8 +42,14 @@ namespace tec {
 			variable& operator=(variable &&obj) = default;
 
 			void modify_name(std::string vname);
-			std::string get_name();
-			void resize_zone(int zone, int _size, dataTypeFlag T = dataTypeFlag::singlePrecision);
+			std::string& get_name();
+
+			void add_zoneData(int zidx, std::vector<float> &&data);
+			void add_zoneData(int zidx, std::vector<double> &&data);
+			void add_zoneData(int zidx, std::vector<int32_t> &&data);
+			void add_zoneData(int zidx, std::vector<int16_t> &&data);
+			void add_zoneData(int zidx, std::vector<uint8_t> &&data);
+			
 			zoneData& operator[](int idx); 
 	};
 
--- src/tec_variable.cpp
+++ src/tec_variable.cpp
@@ -7,21 +7,78 @@ namespace tec {
 	variable::variable() : name("V") {}
 
 	variable::variable(std::string vname) : name(vname) {}
+	
+	variable::variable(std::string vname, std::vector<float> &&data, int zidx) : name(vname) {
+		if(zidx > 0) {
+			add_emptyInstances(zidx);
+		}
+		if(zidx >= 0) {
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+	
+	variable::variable(std::string vname, std::vector<double> &&data, int zidx) : name(vname) {
+		if(zidx > 0) {
+			add_emptyInstances(zidx);
+		}
+		if(zidx >= 0) {
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+	
+	variable::variable(std::string vname, std::vector<int32_t> &&data, int zidx) : name(vname) {
+		if(zidx > 0) {
+			add_emptyInstances(zidx);
+		}
+		if(zidx >= 0) {
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+
+	variable::variable(std::string vname, std::vector<int16_t> &&data, int zidx) : name(vname) {
+		if(zidx > 0) {
+			add_emptyInstances(zidx);
+		}
+		if(zidx >= 0) {
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+	variable::variable(std::string vname, std::vector<uint8_t> &&data, int zidx) : name(vname) {
+		if(zidx > 0) {
+			add_emptyInstances(zidx);
+		}
+		if(zidx >= 0) {
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
 
 	/*
 	variable::variable(const variable &obj) : name(obj.name), subzoneData(obj.subzoneData) {}
 
 	variable::variable(variable &&obj) : name(std::move(obj.name)), subzoneData(std::move(obj.subzoneData)) {}
 	*/
+
 	variable::~variable() {}
+
+	void variable::add_emptyInstances(int numInstances) {
+		for(int i = 0; i < numInstances; i++) {
+			subzoneData.emplace_back(zoneData(dataTypeFlag::singlePrecision));
+		}
+	}
 
 	void variable::resize_zone(int zone, int _size, dataTypeFlag T) {
 		if(zone >= subzoneData.size()) {
+			int N = zone-subzoneData.size();
+			if(N > 0) {
+				add_emptyInstances(N);
+			}
+			/*
 			while(zone > subzoneData.size()) {
 				//push back empty type for zones where the variable is passive/unused 
-				subzoneData.push_back(dataTypeFlag::singlePrecision);
+				subzoneData.emplace_back(dataTypeFlag::singlePrecision);
 			}
-			subzoneData.push_back(T);
+			*/
+			subzoneData.emplace_back(T);
 		}
 		
 		switch(T) {
@@ -49,8 +106,73 @@ namespace tec {
 		name = vname;
 	}
 
-	std::string variable::get_name() {
+	std::string& variable::get_name() {
 		return name;
+	}
+
+	void variable::add_zoneData(int zidx, std::vector<float> &&data) {
+		int N = zidx-subzoneData.size();			
+		if(N < 0) {
+			//SUBZONE DATA ALREADY EXISTS -> OVERWRITE??
+		}
+		else {
+			if(N > 0) {
+				add_emptyInstances(N);
+			}
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+	
+	void variable::add_zoneData(int zidx, std::vector<double> &&data) {
+		int N = zidx-subzoneData.size();			
+		if(N < 0) {
+			//SUBZONE DATA ALREADY EXISTS -> OVERWRITE??
+		}
+		else {
+			if(N > 0) {
+				add_emptyInstances(N);
+			}
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+
+	void variable::add_zoneData(int zidx, std::vector<int32_t> &&data) {
+		int N = zidx-subzoneData.size();			
+		if(N < 0) {
+			//SUBZONE DATA ALREADY EXISTS -> OVERWRITE??
+		}
+		else {
+			if(N > 0) {
+				add_emptyInstances(N);
+			}
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+
+	void variable::add_zoneData(int zidx, std::vector<int16_t> &&data) {
+		int N = zidx-subzoneData.size();			
+		if(N < 0) {
+			//SUBZONE DATA ALREADY EXISTS -> OVERWRITE??
+		}
+		else {
+			if(N > 0) {
+				add_emptyInstances(N);
+			}
+			subzoneData.emplace_back(std::move(data));
+		}
+	}
+
+	void variable::add_zoneData(int zidx, std::vector<uint8_t> &&data) {
+		int N = zidx-subzoneData.size();			
+		if(N < 0) {
+			//SUBZONE DATA ALREADY EXISTS -> OVERWRITE??
+		}
+		else {
+			if(N > 0) {
+				add_emptyInstances(N);
+			}
+			subzoneData.emplace_back(std::move(data));
+		}
 	}
 
 	zoneData& variable::operator[](int zoneIdx) {
